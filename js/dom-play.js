10c15cdd2104f317c3170baeef1e71c95e7bf8be
IriniBahnam <bahnam213@gmail.com>
2022-08-08 09:36:21 -0700

    fixes highlight

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,4 +1,4 @@
-// alert("hi there");
+
 
 const spans = document.querySelectorAll("#play span");
 
@@ -8,43 +8,33 @@ function myAlert(){
     alert("Im clicked");
 }
 
+
 for(const mySpan of spans){
-//mySpan.addEventListener("click", myAlert);
-mySpan.addEventListener("click", function(ev){});
-// alert("The current actor is " + mySpan.dataset.actor);
-highlightActor();
+mySpan.addEventListener("click", function(ev){
+    highlightActor(mySpan.dataset.actor);
+});
+
 }
 
 function highlightActor(actor){
     
     for(const mySpan of spans){
-        if(actor == mySpan.dataset.actor){//current actor, highlight
+        if(actor == mySpan.dataset.actor){
             mySpan.style.backgroundColor='yellow';
-        }else{//other actor, background white
+        }else{
             mySpan.style.backgroundColor='white';
         }
     }
 }
 
-//     if(  el.style.backgroundColor=='yellow'){
-//         el.style.backgroundColor='white';  
-//     }else{
-//     el.style.backgroundColor='yellow';
 
-//     }
-// }
 
 function highlight(el){
 
-    if(  el.style.backgroundColor=='yellow'){
+    if(el.style.backgroundColor=='yellow'){
         el.style.backgroundColor='white';  
     }else{
     el.style.backgroundColor='yellow';
 
     }
 }
-/*
-for (let i =0; i <actors.length; i++) {
-    actors[i].style.backgroundColor = "yellow";
-}
-*/
